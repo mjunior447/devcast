299df3d49dae89c783232bf27339849c69a57aa4
Mauricio Antero <mjunior447@gmail.com>
2024-12-18 22:19:08 -0300

    feat: implement user authentication with Clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,9 @@
 
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
 
 const Home = () => {
-  const tasks = useQuery(api.tasks.get);
+  // const tasks = useQuery(api.tasks.get);
 
   return (
     <div className="mt-9 flex flex-col gap-9">
@@ -14,7 +12,7 @@ const Home = () => {
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
 
         <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
-          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+          {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)} */}
         </div>
 
         <div className="podcast_grid">
