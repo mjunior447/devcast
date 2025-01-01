fcaec2c7eb6dfdc0465651b45a1130f0b4c9c1d1
Mauricio Antero <mjunior447@gmail.com>
2025-01-01 19:28:34 -0300

    feat: create discover page

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,7 +1,68 @@
-const Discover = () => {
+"use client";
+
+import EmptyState from "@/components/EmptyState";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import PodcastCard from "@/components/PodcastCard";
+import Searchbar from "@/components/Searchbar";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { useEffect, useState } from "react";
+
+const Discover = ({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    search: string;
+  }>;
+}) => {
+  const [searchParam, setSearchParam] = useState("");
+
+  useEffect(() => {
+    const getSearchParams = async () => {
+      const search = (await searchParams).search;
+      setSearchParam(search);
+      console.log("search params: ", search);
+    };
+
+    getSearchParams();
+  }, [searchParam, searchParams]);
+
+  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, {
+    search: searchParam || "",
+  });
+
   return (
-    <div>
-      <h1 className="text-20 font-bold text-white-1">Discover</h1>
+    <div className="flex flex-col gap-9">
+      <Searchbar />
+      <div className="flex flex-col gap-9">
+        <h1 className="text-20 font-bold text-white-1">
+          {!searchParam ? "Discover Trending Podcasts" : "Search results for"}{" "}
+          {searchParam && <span className="text-white-2">{searchParam}</span>}
+        </h1>
+        {podcastsData ? (
+          <>
+            {podcastsData.length > 0 ? (
+              <div className="podcast_grid">
+                {podcastsData?.map(
+                  ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                    <PodcastCard
+                      key={_id}
+                      podcastId={_id}
+                      imgUrl={imageUrl!}
+                      title={podcastTitle}
+                      description={podcastDescription}
+                    />
+                  )
+                )}
+              </div>
+            ) : (
+              <EmptyState title="No results found" />
+            )}
+          </>
+        ) : (
+          <LoaderSpinner />
+        )}
+      </div>
     </div>
   );
 };
